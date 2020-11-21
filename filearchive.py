392c07f896b74a39962d5704007d423f311277a3
Alabbas Said Ahmed, Mohamed <malabbas@ae.imshealth.com>
2020-11-21 17:42:28 +0400

    case sensitive ext added

--- filearchive.py
+++ filearchive.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from datetime import datetime
 
-appsPath = "/home/wd3bbas/Downloads/"
+appsPath = "/home/sample/Downloads/"
 todayappsPath = os.path.join(appsPath,str(datetime.date(datetime.now()))+"/")
 
 Rules = [
@@ -29,18 +29,51 @@ Rules = [
     {
         "directory": 'Selenium',
         "ext": ['.side']
-    }
-    ,
+    },
     {
         "directory": 'Python',
         "ext": ['.py']
     }
+    ,
+    {
+        "directory": 'Emails',
+        "ext": ['.msg','.eml']
+    },
+    {
+        "directory": 'Excel',
+        "ext": ['.xlsx']
+    },
+    {
+        "directory": 'MS Word',
+        "ext": ['.docx']
+    },
+    {
+        "directory": 'csv',
+        "ext": ['.csv']
+    },
+    {
+        "directory": 'Images',
+        "ext": ['.png','.jpg','.jpeg','.png']
+    },
+    {
+       "directory": "Database",
+       "ext": ['.sql']
+    },
+    {
+      "directory": "TS",
+      "ext": ['.ts']
+    },
+    {
+     "directory":"Videos",
+     "ext": ['.mkv']
+    }
+
 ]
 
 for filename in os.listdir(appsPath):
     if os.path.isfile(os.path.join(appsPath,filename)):
         for rule in Rules:
-            if Path(filename).suffix in rule['ext']:
+            if Path(filename).suffix.lower() in rule['ext']:
                 if not os.path.exists(os.path.join(todayappsPath,rule['directory'])):
                     os.makedirs(os.path.join(todayappsPath,rule['directory']))
                     os.rename(os.path.join(appsPath,filename),os.path.join(todayappsPath,rule['directory']+'/',filename))
